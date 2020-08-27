7bbb2f11b64ad795d4c6dd8caac6f94b55300a07
Muhummud Faadhil Yusuf <muhummud@webhi.co>
2020-08-27 17:26:23 +0200

    Error handling while loading

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -28,8 +28,10 @@ class TodoApp extends React.Component {
     }
     getTodoItems(){
         axios
+        //.get("/broken-end-point")
         .get("/api/v1/todo_items")
         .then(response => {
+            this.clearErrors();
             this.setState({ isLoading: true });
             const todoItems = response.data;
             this.setState({ todoItems });
@@ -37,7 +39,11 @@ class TodoApp extends React.Component {
         })
         .catch(error => {
             this.setState({ isLoading: true })
-            console.log(error);
+            this.setState({
+                errorMessage: {
+                    message: "There was an error loading your todo items..."
+                }
+            })
         });
     }
     createTodoItem(todoItem) {
